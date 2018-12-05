3bdb385b09e6a7e9f92dec32b1f479ca23a873f9
Angel Kyriako <angelkyriako@gmail.com>
2018-12-05 19:19:34 +0200

    added CanvasHideableUI

---
+++ Assets/@Spinner/Common/CanvasHideableUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CanvasHideableUI : MonoBehaviour {
+
+    [SerializeField] private Canvas _canvas;
+
+    public virtual void Initialize() {
+        if (_canvas == null) {
+            _canvas = GetComponent<Canvas>();
+        }
+        Debug.Assert(_canvas != null, "_canvas should not be null, either add a component or attach an existing one via the inspector.");
+
+        _canvas.enabled = false;
+    }
+
+    public virtual bool IsShowing { get { return _canvas.enabled; } }
+
+    public virtual void Show() {
+        _canvas.enabled = true;
+    }
+
+    public virtual void Hide() {
+        _canvas.enabled = false;
+    }
+}
